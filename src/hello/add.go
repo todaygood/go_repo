28469a406e099ccc435efea51bd533c337d7921a
Margin <jhu_com@163.com>
2017-08-30 04:56:09 +0800

    hello,go

---
+++ src/hello/add.go
@@ -0,0 +1,4 @@
+package add
+
+func add(int 
+
---
+++ src/hello/hello.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+var a,b,c int = 1,2,3
+
+var d,e = 13,"go"
+
+func main(){
+    fmt.Printf("Hello world ,你好，世界!\n")
+}
+
